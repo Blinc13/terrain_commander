16fd8677348eab83775a6dcb7faba7d52a5ad546
Blinc <65546512+Blinc13@users.noreply.github.com>
2022-09-24 15:45:09 +0300

    Working level selector

--- src/ui/main_menu.gd
+++ src/ui/main_menu.gd
@@ -13,7 +13,22 @@ func play():
 
 
 onready var embeded_levels = $LevelsList/TabContainer/Embeded
-onready var custom_levels = $LevelsList/TabContainer/Custom
+#onready var custom_levels = $LevelsList/TabContainer/Custom
+
+var embeded_levels_list = Array()
+#var custom_levels_list = Array()
+
+func add_embeded_level(name: String, path: String):
+	embeded_levels.add_item(name)
+	embeded_levels_list.push_back(path)
 
 func init_levels_list():
-	embeded_levels.add_item("Test scene")
+	add_embeded_level("Test level", "res://levels/TestLevel.tscn")
+
+func embeded_level_selected(index):
+	var level_scene = load(embeded_levels_list[index]) as PackedScene
+	var level = level_scene.instance()
+	
+	visible = false
+	
+	get_parent().add_child(level)
