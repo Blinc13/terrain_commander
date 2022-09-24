f0b82f54d15d6b250fee6408ab05bc24a3765b6a
Blinc <65546512+Blinc13@users.noreply.github.com>
2022-09-24 13:49:11 +0300

    Update terrain material

--- src/ui/main_menu.gd
+++ src/ui/main_menu.gd
@@ -1,8 +1,19 @@
 extends Control
 
+onready var levels_list = $LevelsList
 
 func quit():
 	get_tree().quit(0)
 
 func play():
-	pass # TODO: Finish script
+	init_levels_list()
+	
+	levels_list.show()
+
+
+
+onready var embeded_levels = $LevelsList/TabContainer/Embeded
+onready var custom_levels = $LevelsList/TabContainer/Custom
+
+func init_levels_list():
+	embeded_levels.add_item("Test scene")
